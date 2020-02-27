2faeaa9f326eca523607922ada21ad257f014805
Tucker Miller <tdmiller7@bsu.edu>
2020-02-27 14:26:00 -0500

    Inital Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component{
+
+  render () {
+    return (
+      <div className="App">
+      <div className="App-header">
+        <h1>BO LIVE</h1>
+        <img src="http://bo.tuckermillerdev.com/"></img>
+      </div>
+    </div>
+    )
+  }
+}
