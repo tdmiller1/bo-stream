b711d9444396220da9923f03ae41ada15182ec16
Tucker Miller <tdmiller7@bsu.edu>
2020-02-27 15:02:49 -0500

    bo_ip

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ export default class App extends Component{
 
 
   render () {
+    console.log(process.env.BO_IP);
     return (
       <div className="App">
       <div className="App-header">
         <h1>BO LIVE</h1>
-        <img src={process.env.IP}></img>
+        <img src={process.env.BO_IP}></img>
       </div>
     </div>
     )
