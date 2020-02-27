513630252c0497a5c537054ed326f7d075ecbeec
Tucker Miller <tdmiller7@bsu.edu>
2020-02-27 14:44:48 -0500

    Second Draft

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 
 export default class App extends Component{
 
+
   render () {
     return (
       <div className="App">
       <div className="App-header">
         <h1>BO LIVE</h1>
-        <img src={process.env.BO_IP}></img>
+        <img src={process.env.NODE_ENV == 'production' ? process.env.IP : 'undefined'}></img>
       </div>
     </div>
     )
